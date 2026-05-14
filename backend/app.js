c4edb84f72fccb28ae0239eef209dca33e7c83bc
Lucas Soares <147398076+LucasAlface@users.noreply.github.com>
2026-05-14 16:04:21 +0000

    adicao das routes a app

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,20 @@
 const express = require("express");
 const path = require("path");
 require('dotenv').config({ path: path.resolve(__dirname, "../.env") });
-const { TipoEcoponto, TipoDeposito, Deposito, Ecoponto, Equipamento, EcopontoEquipamento, EcopontoLogs } = require("./models/models");
 const PORT = process.env.PORT || 3000;
 const { inserirDados } = require("./script");
-const tipo_ecoponto_router = require("./routes/tipoEcoponto")
+const {
+  tipo_ecoponto_router,
+  tipo_deposito_router,
+  deposito_router,
+  ecoponto_router,
+  equipamento_router,
+  ecoponto_equipamento_router
+} = require("./routes/routes");
 
 const app = express();
 
 app.use(express.json());
-
-const tabelas = {
-  "tipo_ecoponto": TipoEcoponto,
-  "tipo_deposito": TipoDeposito,
-  "deposito": Deposito,
-  "ecoponto": Ecoponto,
-  "equipamento": Equipamento,
-  "ecoponto_equipamento": EcopontoEquipamento,
-  "ecoponto_logs": EcopontoLogs
-}
 
 app.get("/", async (req, res) => {
   try {
@@ -30,140 +26,11 @@ app.get("/", async (req, res) => {
 });
 
 app.use("/tipoecoponto", tipo_ecoponto_router);
-
-app.post("/inserir/:nometabela", async (req, res) => {
-  try {
-    const dados = req.body;
-    const { nometabela } = req.params;
-
-    const tabela = tabelas[nometabela];~
-    console.log(tabela);
-    if (!tabela) {
-      return res.status(400).json({ erro: "Tabela inválida" });
-    }
-    await tabela.bulkCreate(dados);
-    res.json("Registro criado com sucesso");
-  } catch (err) {
-    res.status(500).json({ erro: err.message });
-  }
-});
-
-app.put("/atualizar/:nometabela/:id", async (req, res) => {
-  try {
-    const dados = req.body;
-    const { nometabela, id } = req.params;
-
-    const tabela = tabelas[nometabela];
-    if (!tabela) {
-      return res.status(400).json({ erro: "Tabela inválida" });
-    }
-    const result = await tabela.update(dados, { where: { id: id } });
-
-    if (result[0] === 0) {
-      return res.status(404).json({ erro: "Registro não encontrado" });
-    }
-
-    res.json("Registro atualizado com sucesso");
-  } catch (err) {
-    res.status(500).json({ erro: err.message });
-  }
-});
-
-app.put("/atualizar/:nometabela/:id1/:id2", async (req, res) => {
-  try {
-    const dados = req.body;
-    const { nometabela, id1, id2 } = req.params;
-
-    const tabela = tabelas[nometabela];
-    if (!tabela) {
-      return res.status(400).json({ erro: "Tabela inválida" });
-    }
-    const result = await tabela.update(dados, { where: { ecopontoId: id1, equipamentoId: id2 } });
-
-    if (result[0] === 0) {
-      return res.status(404).json({ erro: "Registro não encontrado" });
-    }
-
-    res.json("Registro atualizado com sucesso " + result.rowCount + " colunas afetadas");
-  } catch (err) {
-    res.status(500).json({ erro: err.message });
-  }
-});
-
-app.delete("/apagar/:nometabela/:id", async (req, res) => {
-  try {
-    const { nometabela, id } = req.params;
-
-    const tabela = tabelas[nometabela];
-    if (!tabela) {
-      return res.status(400).json({ erro: "Tabela inválida" });
-    }
-   
-    await tabela.destroy({ where: { id: id } });
-
-    res.json("Registro apagado com sucesso");
-  } catch (err) {
-    res.status(500).json({ erro: err.message });
-  }
-});
-
-app.delete("/apagar/:nometabela/:id1/:id2", async (req, res) => {
-  try {
-    const { nometabela, id1, id2 } = req.params;
-
-    const tabela = tabelas[nometabela];
-    if (!tabela) {
-      return res.status(400).json({ erro: "Tabela inválida" });
-    }
-    
-    await tabela.destroy({ where: { ecopontoId: id1, equipamentoId: id2 } });
-
-    res.json("Registro apagado com sucesso");
-  } catch (err) {
-    res.status(500).json({ erro: err.message });
-  }
-});
-
-app.put("/capacidade", async (req, res) => {
-  try {
-    const { codigoEquipamento, profundidade } = req.body;
-
-    const equipamento = await Equipamento.findOne({ where: { codigo: codigoEquipamento } });
-    if (!equipamento) {
-      return res.status(404).json({ erro: "Equipamento não encontrado" });
-    }
-    const equipamentoId = equipamento.id;
-
-    const ecopontoEquipamento = await EcopontoEquipamento.findOne({ where: { equipamentoId: equipamentoId, ativo: true } });
-    if (!ecopontoEquipamento) {
-      return res.status(404).json({ erro: "Ecoponto associado ao equipamento não encontrado" });
-    }
-    const ecopontoId = ecopontoEquipamento.ecopontoId;
-
-    const ecoponto = await Ecoponto.findByPk(ecopontoId);
-    if (!ecoponto) {
-      return res.status(404).json({ erro: "Ecoponto não encontrado" });
-    }
-
-    const deposito = await Deposito.findByPk(ecoponto.depositoId);
-    if (!deposito) {
-      return res.status(404).json({ erro: "Depósito associado ao ecoponto não encontrado" });
-    }
-
-    const capacidadeTotal = deposito.capacidadeTotal;
-    const altura = deposito.altura;
-
-    const percentagem = profundidade / altura;
-    const capacidadeAtual = percentagem * capacidadeTotal;
-
-    await ecoponto.update({ capacidadeAtual: capacidadeAtual }); 
-
-
-    res.json("simmmm");
-  } catch (err) {
-    res.status(500).json({ erro: err.message });
-  }
-});
+app.use("/tipodeposito", tipo_deposito_router);
+app.use("/deposito", deposito_router);
+app.use("/ecoponto", ecoponto_router);
+app.use("/equipamento", equipamento_router);
+app.use("/ecopontoequipamento", ecoponto_equipamento_router);
 
 app.listen(PORT, () => {
   console.log("Servidor online");
---
+++ backend/routes/routes.js
@@ -0,0 +1,15 @@
+const tipo_ecoponto_router = require("./tipoEcoponto")
+const tipo_deposito_router = require("./tipoDeposito")
+const deposito_router = require("./deposito")
+const ecoponto_router = require("./ecoponto")
+const equipamento_router = require("./equipamento")
+const ecoponto_equipamento_router = require("./ecopontoEquipamento")
+
+module.exports = {
+  tipo_ecoponto_router,
+  tipo_deposito_router,
+  deposito_router,
+  ecoponto_router,
+  equipamento_router,
+  ecoponto_equipamento_router,
+};
